8b8b92d8d7b5d91415aec463d368abe7db4a90af
Joao Vitor Pietsiaki Moraes <joao.moraes@lactec.org.br>
2013-07-15 16:55:29 -0300

    AsyncStatus enum review.

--- WinRT.NET/Foundation/AsyncStatus.cs
+++ WinRT.NET/Foundation/AsyncStatus.cs
@@ -24,8 +24,10 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+using Windows.Foundation.Metadata;
 namespace Windows.Foundation
 {
+	[Version(WindowsVersion.NTDDI_WIN8)]
 	public enum AsyncStatus
 	{
 		/// <summary>
